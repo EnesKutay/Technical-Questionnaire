11d28f1b988fae65e1f5d8a54238d4c403ca1341
kutay <61122519+EnesKutay@users.noreply.github.com>
2023-03-14 02:00:18 +0300

    Update question8.py

--- question8.py
+++ question8.py
@@ -1,3 +1,18 @@
+"""
+We can use regular expressions to extract the required information.
+
+We create a dictionary url_dict to store the extracted URLs for each device type.
+
+We define a regular expression pattern url_pattern to match the URL. The pattern matches the <url> XML tags, an optional protocol part (https:// or http://), and the pure URL information that we are interested in. The pure URL information can have alpha-numeric, case-insensitive characters, underscore (_) character and dot (.) character only.
+
+We iterate over each row in the database and apply the regular expression pattern to the Stats_Access_Link column using the re.search() function. If the regular expression matches, we extract the URL from the second group of the match (match.group(2)), which corresponds to the pure URL information that we are interested in.
+
+We add the extracted URL to the dictionary url_dict for the corresponding device type. If the device type already exists in the dictionary, we append the URL to its list of URLs. If the device type does not exist in the dictionary, we create a new key-value pair with the device type as the key and a list containing the URL as the value.
+
+Finally, we print the extracted URLs for each device type using a for loop and the items() method of the url_dict dictionary. We use the join() method to join the URLs in the list into a single string with a comma separator.
+
+"""
+
 import re
 
 # You have to provide database in csv format with pandas
